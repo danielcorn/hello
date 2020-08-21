fcf7cdeb6bc549e644552d57ec0f05ab7502237f
Daniel Cornelius <cornelius.daniel96@husker.unl.edu>
2020-08-21 00:03:28 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Daniel Cornelius
+ * Date: 2020/08/18
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Daniel\n");
+
+  return 0;
+}
